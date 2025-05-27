20d63d0fec926a2ca7517c3edc09ec1c0351d9c0
44takaoki <yoshitakaoki2021@gmail.com>
2025-05-27 19:45:25 +0900

    課題3実装

--- src/components/PostDetailPage.js
+++ src/components/PostDetailPage.js
@@ -1,10 +1,12 @@
-import { useParams } from "react-router-dom";
-import { posts } from "../data/posts";
+import { useLocation, useParams } from "react-router-dom";
 import PostDetail from "./PostDetail";
 
 export default function PostDetailPage() {
   // ルートパラメータを取得
   const { id } = useParams();
+  const { state: posts } = useLocation();
+  // console.log(posts);
+
   return (
     <div>
       <PostDetail posts={posts} id={id} />
